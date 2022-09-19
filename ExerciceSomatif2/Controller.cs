c0b3bcc0a3665275280bdb2258e039cce363de80
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2022-09-19 16:34:11 +0000

    Add files via upload

---
+++ ExerciceSomatif2/Controller.cs
@@ -0,0 +1,59 @@
+﻿using ExerciceSomatif2.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle = ExerciceSomatif2.Models.Rectangle;
+
+//using Rectangle = ExerciceSomatif2.Modelse;
+
+namespace ExerciceSomatif2
+{
+    public class Controller
+    {
+        //        random = private new Random();
+
+        private List<Forme> formes;
+        private int idForme;
+        private Forme formeCourant;
+        private string type;
+
+        public Controller()
+        {
+            Formes = new List<Forme>();
+            Formes.Add(new Rectangle(Color.Green));
+            Formes.Add(new RectanglePlein(Color.Green));
+            Formes.Add(new Cercle(Color.Orange));
+            Formes.Add(new CerclePlein(Color.Green));
+
+            IdForme = 0;
+            FormeCourant = Formes[IdForme];
+            MessageBox.Show(Formes[IdForme].ObtenirForme());
+        }
+
+        public Forme Forme { get => FormeCourant; }
+        public string Type { get => Type1; }
+        public List<Forme> Formes { get => formes; set => formes = value; }
+        public int IdForme { get => idForme; set => idForme = value; }
+        public Forme FormeCourant { get => formeCourant; set => formeCourant = value; }
+        public string Type1 { get => type; set => type = value; }
+
+        public event EventHandler<Forme> ChangementFormeCourantEvent;
+
+        public event EventHandler<string> ChangementFormeCourantTypeEvent;
+
+        public void ChoisirForme()
+        {
+            Forme nouvelleForme = formes[idForme];
+            if (formeCourant != nouvelleForme)
+            {
+                return;
+            }
+            formeCourant = nouvelleForme;
+            formeCourant.Dessiner();
+            ChangementFormeCourantEvent?.Invoke(this, formeCourant);
+        }
+    }
+}
